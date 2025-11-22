982f001a7d0725e366753841c8cbbcabde48cdd1
Keshav Agrawal <keshavagrawal21@gmail.com>
2025-11-22 18:25:10 +0100

    output change

--- infer_dtc_qwen.py
+++ infer_dtc_qwen.py
@@ -159,13 +159,13 @@ def eval_model(args):
 
         elapsed = time.time() - start
 
-        print(output_ids)
+        print(output_ids.shape)
         
         response = processor.batch_decode(
             output_ids,
             skip_special_tokens=True,
-        )
-        
+        )[0].strip()
+
         if idx < 2:
             print("\n========== SAMPLE OUTPUT ==========")
             print("Image:", img_path)
